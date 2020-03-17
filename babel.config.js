9d7e95b4194bd3f69229e861a9206f60dffab87c
Luong Thanh Tung <tunglt@tunglt.itech.com.vn>
2020-03-17 15:38:23 +0700

    change build config

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,13 @@
 module.exports = {
-    plugins: ["@babel/plugin-proposal-class-properties"],
-    env: {
-        test: {
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        modules: 'commonjs',
-                        debug: false
-                    },
-                ],
-                '@babel/preset-react'
-            ],
-            plugins: [
-                '@babel/plugin-proposal-object-rest-spread',
-                '@babel/plugin-syntax-dynamic-import',
-                '@babel/plugin-transform-regenerator',
-                '@babel/plugin-transform-runtime'
-            ],
-        },
-        production: {
-            presets: [
-                // WebPack handles ES6 --> Target Syntax
-                ['@babel/preset-env', {modules: false}],
-            ],
-            ignore: ['**/*.test.jsx', '**/*.test.js', '__snapshots__', '__tests__'],
-        },
-        development: {
-            presets: [
-                // WebPack handles ES6 --> Target Syntax
-                ['@babel/preset-env', {modules: false}],
-            ],
-            ignore: ['**/*.test.jsx', '**/*.test.js', '__snapshots__', '__tests__']
-        }
-    }
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-regenerator',
+        '@babel/plugin-transform-runtime'
+    ],
+    presets: [
+        // WebPack handles ES6 --> Target Syntax
+        ['@babel/preset-env', {modules: false, useBuiltIns: 'entry', corejs: 3}],
+    ],
 };
