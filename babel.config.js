961cc04cda969dbb05b51b5bf8340e37b0b1e3b8
TungLT <tungluong2809@gmail.com>
2020-04-08 09:08:03 +0700

    Update logic for CommandsManager.js, support binding/unbind multi keys to one command

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
-        '@babel/plugin-proposal-class-properties',
+        ["@babel/plugin-proposal-class-properties", { "loose": true }],
         '@babel/plugin-proposal-object-rest-spread',
         '@babel/plugin-syntax-dynamic-import',
         '@babel/plugin-transform-regenerator',
